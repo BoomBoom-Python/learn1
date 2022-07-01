d79bea85067ad7d2918c79fefe70cb0fd1f6a75c
BoomBoom <patelvivek598@gmail.com>
2022-07-01 12:44:11 +0530

    firt commit

---
+++ functions in python.py
@@ -0,0 +1,53 @@
+# name = "sneha"
+#
+# def myfunc():
+#   print("my name is " +name)
+#
+# myfunc()
+# print("after function")
+
+# name1 = "sneha"
+
+# def myfunc(name2):
+#     print("hello " +name2)
+#
+# myfunc("abhijeet")
+# myfunc("kritik")
+# myfunc("vivek")
+# myfunc("sarang")
+# myfunc("joy")
+#
+# def sum(numbers):
+#   total = 0
+#   for x in numbers:   #8,5,2,3
+#     total = total + x
+#   return total
+#
+#
+# print(sum([8, 2, 3, 0, 7]))
+
+
+# def string_reverse(str1):
+#   rstr1 = ''
+#   index = len(str1)
+#   while index > 0:
+#     rstr1 = rstr1 + str1[index - 1]
+#     index = index - 1
+#   return rstr1
+#
+#
+# print(string_reverse('1234abcd'))
+#
+# #
+# def max_of_two(x, y, z):
+#   if x > y and x>z:
+#     return x
+#   elif z > y and z>x:
+#     return z
+#   return y
+#
+#
+#
+# print(max_of_two(8, 16, -5))
+#
+#
